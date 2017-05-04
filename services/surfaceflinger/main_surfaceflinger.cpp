813f4af7eadff3d07f3ae79b70146fb380eb390d
Shilin Victor <chrono.monochrome@gmail.com>
2017-05-04 10:26:21 +0300

    frameworks-native_WW14-RT-sched-priorities.patch
    Change-Id: I7da5cdbb57464a350b643d65bb53e38929b3a2a5

--- services/surfaceflinger/main_surfaceflinger.cpp
+++ services/surfaceflinger/main_surfaceflinger.cpp
@@ -64,7 +64,7 @@ int main(int, char**) {
 
 #ifndef HARDWARE_SCHED_FIFO
     struct sched_param param = {0};
-    param.sched_priority = 2;
+    param.sched_priority = 4;
     if (sched_setscheduler(0, SCHED_FIFO, &param) != 0) {
         ALOGE("Couldn't set SCHED_FIFO");
     }
