146d2094fa05902beb2847251402889a38d487a6
Shilin Victor <chrono.monochrome@gmail.com>
2017-09-12 00:36:48 +0300

    libui: fix unresolved symbol GraphicBufferAllocator::alloc
    Change-Id: Ib696caa6b205c5ecd71ae6ca9b3505b39ba0820e

--- libs/ui/GraphicBufferAllocator.cpp
+++ libs/ui/GraphicBufferAllocator.cpp
@@ -158,3 +158,6 @@ status_t GraphicBufferAllocator::free(buffer_handle_t handle)
 
 // ---------------------------------------------------------------------------
 }; // namespace android
+
+__strong_alias(_ZN7android22GraphicBufferAllocator5allocEjjiiPPK13native_handlePi,
+        _ZN7android22GraphicBufferAllocator5allocEjjijPPK13native_handlePj);
